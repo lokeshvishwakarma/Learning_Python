49aada4ab25e65959cecd512c64246a9d79ee027
lokeshvishwakarma <uvishwakarma3@gmail.com>
2020-01-05 18:11:17 +0530

    Added Exercises to chapter 03

---
+++ Chapter 03_Statements_and_Syntax/5_Exercise/exercise02.py
@@ -0,0 +1,34 @@
+"""
+1. Open the exam.txt file withonly readd permission and store the contents in a list called
+exam_lines.
+"""
+
+"""
+2. How many lines does the file 'exam.txt' have?
+"""
+
+"""
+3. Print out the 5th line of the file.
+"""
+
+"""
+4. Grab the last line of the text file and save it to a variable called last.
+"""
+
+"""
+5. Use indexing to grab letter 'o' from the last line of the file.
+"""
+
+"""
+6. How could you use Python to count how many words are there in the last line?
+"""
+
+"""
+7. Retrieve the string 'get me please' from the below dictionary. 
+    D = {"levelone": [1, 2, {"leveltwo": [5, 6, [1, ['get me please']]]}]}
+"""
+
+"""
+8. How many unique integers are there in the following list?
+    mylist = [1, 2, 3, 4, 5, 6, 7, 3, 4, 5,6, 2, 4, 3, 5, 9, 8, 7, 6, 8, 4, 5, 6, 8, 3, 5, 2, 8, 1, 4, 6]
+"""
